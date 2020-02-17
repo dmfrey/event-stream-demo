686d0f4648784a07e1bdfe231fd76b8e4fcd754c
Daniel Frey <dfrey@pivotal.io>
2020-02-17 11:25:19 -0500

    replace fabric8 with jib, setup instructions for various node environments

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM adoptopenjdk/openjdk11:jdk-11.0.6_10-alpine
-RUN mkdir /opt/app
-EXPOSE 8080
-COPY target/event-stream-demo-0.0.1-SNAPSHOT.jar /opt/app
-CMD ["java", "-jar", "/opt/app/event-stream-demo-0.0.1-SNAPSHOT.jar"]
+VOLUME /tmp
+COPY target/event-stream-demo-0.0.1-SNAPSHOT.jar app.jar
+CMD ["java", "-jar", "/app.jar"]
--- src/main/java/io/pivotal/dmfrey/config/SecurityConfig.java
+++ src/main/java/io/pivotal/dmfrey/config/SecurityConfig.java
@@ -1,24 +1,24 @@
-package io.pivotal.dmfrey.config;
-
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure( HttpSecurity http ) throws Exception {
-
-        http
-                .authorizeRequests()
-                    .requestMatchers( PathRequest.toStaticResources().atCommonLocations(), EndpointRequest.toAnyEndpoint() ).permitAll()
-                    .anyRequest().permitAll();
-
-    }
-
-}
+//package io.pivotal.dmfrey.config;
+//
+//import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure( HttpSecurity http ) throws Exception {
+//
+//        http
+//                .authorizeRequests()
+//                    .requestMatchers( PathRequest.toStaticResources().atCommonLocations(), EndpointRequest.toAnyEndpoint() ).permitAll()
+//                    .anyRequest().permitAll();
+//
+//    }
+//
+//}
